2f38e3d58d2f16b3e416fa6d56a981030519c040
victoire_clairon@naver.com <victoire_clairon@naver.com>
2021-12-24 17:16:24 +0900

    카카오톡 URL 등록

--- js/share.js
+++ js/share.js
@@ -1,14 +1,14 @@
 // TO DO : 배포 url 추가
 // TO DO : 카카오톡 API > 내 어플리케이션 > 플랫폼 > 웹 플랫폼 등록
-const sendUrl = '';
+const sendUrl = 'https://www.wordsquare.org';
 
 function shareKakao(){
   var resultImg = document.querySelector('#resultImg');
   var resultAlt = resultImg.firstElementChild.alt;
   const shareTitle = '성경 속 나와 닮은 인물 찾기';
   const shareDes = infoList[resultAlt].name;
-  const shareImage = sendUrl + './images/resultSheet/result' + resultAlt + '.png';
-  const shareURL = sendUrl + './result' + resultAlt + '.html';
+  const shareImage = sendUrl + 'images/resultSheet/result' + resultAlt + '.png';
+  const shareURL = sendUrl + 'result' + resultAlt + '.html';
 
   Kakao.Link.sendDefault({
     objectType: 'feed',
